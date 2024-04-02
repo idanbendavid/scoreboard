b83d28d950f8c51cea5df42809e206482fc246f0
idanbendavid <idanbendavid10@hotmail.com>
2024-04-02 14:01:24 +0300

    adding loading sign with activity indicator

--- src/components/common/lazyLoading.js
+++ src/components/common/lazyLoading.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, ImageBackground, StyleSheet } from 'react-native';
+import { View, ImageBackground, StyleSheet, ActivityIndicator } from 'react-native';
 
 const LazyLoadingImage = ({ source }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -9,18 +9,23 @@ const LazyLoadingImage = ({ source }) => {
   };
 
   return (
-    <View resizeMode={'contain'}>
-      <ImageBackground source={source} style={styles.image} onLoad={handleImageLoad} />
-    </View>
+    <>
+      {isLoading &&
+        <ActivityIndicator/>
+      }
+      <View resizeMode={'contain'}>
+        <ImageBackground source={source} style={styles.image} onLoad={handleImageLoad} />
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   image: {
     position: 'absolute',
-    width: '100%', 
+    width: '100%',
     height: undefined,
-    aspectRatio:2
+    aspectRatio: 2
   }
 })
 export default LazyLoadingImage;
